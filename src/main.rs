bf9a6ecf62af369746c5e1d0efccae148230f2ce
riccardodebenedictis <riccardo.debenedictis@cnr.it>
2026-08-10 12:07:01 +0200

    Update main function to enhance usage message and support reading from stdin

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,24 @@
 use std::env;
+use std::io::{self, Read};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+
     if args.len() < 2 {
-        eprintln!("Usage: semitone <file.smt2>");
-        return;
+        eprintln!("Usage: semitone <file.smt2 | ->");
+        std::process::exit(1);
     }
 
+    let file_path = &args[1];
     let mut stdout = std::io::stdout();
     let mut runner = semitone::parser::SmtParser::new(&mut stdout);
-    runner.run_file(&args[1]);
+
+    if file_path == "-" {
+        let mut buffer = String::new();
+        io::stdin().read_to_string(&mut buffer).expect("Failed to read from stdin");
+        runner.run_str(&buffer);
+    } else {
+        // Altrimenti, apri normalmente il file
+        runner.run_file(file_path);
+    }
 }
