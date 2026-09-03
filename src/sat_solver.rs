784624c5f4a91e0157e79643a06adf5f15a0ccb3
Riccardo De Benedictis <riccardo.debenedictis@cnr.it>
2026-09-03 16:36:51 +0200

    Refactor error handling in propagate method for simplified literals

--- src/sat_solver.rs
+++ src/sat_solver.rs
@@ -285,12 +285,8 @@ impl SatSolver {
 
                 if self.lit_value(simplified_lits[0]) == Some(false) {
                     return Err(simplified_lits);
-                } else if self.lit_value(simplified_lits[1]) == Some(false) {
-                    if self.lit_value(simplified_lits[0]) == None {
-                        if !self.enqueue(simplified_lits[0], Some(clause_index)) {
-                            return Err(simplified_lits);
-                        }
-                    }
+                } else if self.lit_value(simplified_lits[1]) == Some(false) && self.lit_value(simplified_lits[0]).is_none() && !self.enqueue(simplified_lits[0], Some(clause_index)) {
+                    return Err(simplified_lits);
                 }
             }
         }
