42a84d6c077b654e6039b8243330b78efde56372
riccardodebenedictis <riccardo.debenedictis@cnr.it>
2026-09-21 22:15:54 +0200

    Remove unnecessary integer check in LraTheory's row validation logic

--- src/lra_theory.rs
+++ src/lra_theory.rs
@@ -370,11 +370,6 @@ impl LraTheory {
             let mut explanation = Vec::new();
 
             for (nb_var, nb_coeff) in row.iter() {
-                if !self.ints[*nb_var] {
-                    valid_row = false;
-                    break;
-                }
-
                 let nb_val = self.value(*nb_var);
 
                 if !nb_val.infinitesimal_part().is_zero() {
